74bc2a7e92bf03f32454261338e64d23baa724b0
Zane <lyzane.it@gmail.com>
2017-07-11 13:51:46 +0800

    添加了母版页

--- Zane.LogHub.Web/Startup.cs
+++ Zane.LogHub.Web/Startup.cs
@@ -55,7 +55,7 @@ namespace Zane.LogHub.Web
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Dashboard}/{action=Index}/{id?}");
             });
         }
     }
